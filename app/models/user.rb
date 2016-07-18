02bc16c895c876d1a79fe12ea7972f32016ca69a
Jimmy Ranger <jranger@me.com>
2016-07-18 16:00:57 -0400

    fixes regexp for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates_uniqueness_of :email, case_sensitive: false
-  validates_format_of :email, with: Devise::email_regexp
+  validates_format_of :email, with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :description, length: { maximum: 140 }
 
