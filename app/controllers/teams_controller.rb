a9f9663810e7bcc01f50880991ac7a4195d8014a
Jimmy Ranger <jranger@me.com>
2016-07-18 16:30:04 -0400

    adds new and create to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,29 @@
 class TeamsController < ApplicationController
 
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(new_team_params)
+
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      flash[:error] = "Please try again!"
+      render :new
+    end
+  end
+
   def show
     @team = Team.find(params[:id])
     @organization = @team.organization
+  end
+
+  protected
+
+  def new_team_params
+    params.require(:team).permit(:name, :description, :city, :state, :country, :timezone, :organization_id)
   end
 
 end
