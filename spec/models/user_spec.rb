5ccfc1295e47061727ac8608156b1efbdb72d7a6
Jimmy Ranger <jranger@me.com>
2016-07-18 15:34:52 -0400

    adds model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe User, type: :model do
 
   it { should have_valid(:description).when("Transfiguration Professor, Deputy Headmistress", nil) }
   it { should_not have_valid(:description).when("Professor Albus Percival Wulfric Brian Dumbledore, Order of Merlin (first class), Headmaster of Hogwarts School of Witchcraft and Wizardry, Supreme Mugwump of the International Confederation of Wizards, and Chief Warlock of the Wizengamot.")}
+
+  describe "#fullname" do
+    it 'returns the users first and last name as a single string' do
+      user = create(:user)
+      expect(user.fullname).to eq('Rubeus Hagrid')
+    end
+  end
 end
