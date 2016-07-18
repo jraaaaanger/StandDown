fb5ad8b26ecde54f7f813d341879264136059279
Jimmy Ranger <jranger@me.com>
2016-07-18 13:14:10 -0400

    begins to set up new user

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -13,4 +13,17 @@ class OrganizationsController < ApplicationController
     @organization = Organization.new
   end
 
+  def create
+    @organization = Organization.new(params[:id])
+    @organization.creator = current_user
+
+    if @organization.save
+      redirect_to @organization
+    else
+      flash[:alert] = 'Something went wrong. Please try again!'
+      render :new
+    end
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+  
   root "organizations#index"
 
-  resources :users, only: [:show, :destroy]
   resources :teams
   resources :organizations
 end
