227b20b277b08aaf45f62add37ecebec54614809
Jimmy Ranger <jranger@me.com>
2016-07-20 10:36:28 -0400

    adds question tests

---
+++ spec/features/questions/01_user_sees_questions_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'user sees questions that need to be answered' do
+  context 'before signing in' do
+    let!(:question) { create(:question) }
+    scenario 'user does not see questions yet' do
+      visit root_path
+
+      expect(page).to_not have_content(question)
+    end
+  end
+
+  context 'after signing in' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question) }
+
+    before(:each) do
+      login_as user
+      visit root_path
+    end
+
+    scenario 'user sees questions on the root path' do
+        expect(page).to have_content(question.body)
+    end
+
+    scenario 'user clicks on question to answer it' do
+      click_link question.body
+
+      expect(page).to have_content(question.body)
+      expect(page).to have_css('form')
+      expect(page).to have_button('Submit')
+    end
+  end
+end
