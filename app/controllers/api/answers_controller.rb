0c62e01fc6cac8556add37cb15908e520cc72300
Jimmy Ranger <jranger@me.com>
2016-07-21 10:34:36 -0400

    plays with answers api

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -3,4 +3,22 @@ class Api::AnswersController < ApplicationController
     @answers = Answer.find(params[:answer_id])
     render json: @answer
   end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new
+    render json: @answer
+  end
+
+  def index
+    @answer = Answer.new
+  end
+
+  protected
+
+  def new_answer_params
+  end
 end
