ef00538797077db7f0c2a230a6a2c3279c0fc9a4
Jimmy Ranger <jranger@me.com>
2016-07-23 14:08:46 -0400

    lots of JS, not working yet

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -11,16 +11,32 @@ function showUsers() {
 }
 
 function showRecentAnswer(newInfo) {
-  $recentDiv = $('#recent-activity');
-  $recentDiv.append($userName);
+  $recentDiv = $('#recent-activity-appendable');
+  $lastAdded = $($recentDiv).find('.user-response').last()[0];
+
+  $toHide = $recentDiv.find('.user-response')[5];
+  $responderTime = newInfo.time;
+  $userLink = '<a href="mailto:' + newInfo.user.email + '">' + newInfo.user.name + '</a>';
+  $userDiv = '<div class="left-section">' + $userLink + " | " + $responderTime + " there</div>";
+
+  $questionBody = '<span class="team-question">'+ newInfo.body.question +'</span>';
+  $answerBody = '<span class="team-answer">'+ newInfo.body.answer +'</span>'
+  $answerDiv = '<div class="right-section">'+ $questionBody + $answerBody +'</div>';
+
+  $fullDiv = '<div class="user-response">' + $userDiv + $answerDiv + '</div>';
+  $recentDiv.prepend($fullDiv);
 }
 
 function liveFeed() {
   var teamID = $('#team-id').text();
   var path = '/api/teams/' + teamID + '/answers.json';
+  var oldAnswerId;
 
   var response = $.get(path, function(data) {
-    $lastAnswer = data[data.length - 1];
-    showRecentAnswer($lastAnswer);
+    $lastAnswer = data.answers[data.answers.length - 1];
+  
+    showRecentAnswer($lastAnswer, oldAnswerId)
+    setTimeout(liveFeed, 1000);
+    console.log('done');
   }, "json")
 }
--- app/views/api/answers/index.json.jbuilder
+++ app/views/api/answers/index.json.jbuilder
@@ -1,1 +1,12 @@
-HELLO
+json.answers @answers do |a|
+  json.id a.id
+  json.time a.submitter_time
+  json.body do
+    json.question a.question.body
+    json.answer a.body
+  end
+  json.user do
+    json.name a.user.fullname
+    json.email a.user.email
+  end
+end
