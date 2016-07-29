9de9dcacc3841b7c9c46eec1afeceec970fc4fcc
Jimmy Ranger <jranger@me.com>
2016-07-29 12:58:53 -0400

    updates tests

---
+++ spec/features/organizations/02_user_sees_org_info_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+  context 'as an authenticated user' do
+    let!(:organization) { create(:organization) }
+    let!(:team) { create(:team, organization: organization) }
+    let!(:question) { create(:question, organization: organization) }
+    let!(:new_question) { create(:question, organization: organization, body: 'How are you?') }
+    let!(:user) { create(:user, team: team) }
+    let!(:new_team) { create(:team, organization: organization, name: 'Gryffindor') }
+    let!(:answer) { create(:answer, question: question, user: user) }
+
+    before(:each) do
+      login_as user
+      visit root_path
+    end
+
+    scenario 'user sees organization answers live feed on home' do
+
+      expect(page).to have_content('Recent Activity')
+      expect(page).to have_content(answer.body)
+    end
+
+    scenario 'user sees recent activity live update without reload', js: true do
+      click_link new_question.body
+      fill_in 'answer', with: "I am fine"
+      click_link 'Answer'
+
+      expect(page).to have_content('I am fine')
+    end
+
+    scenario 'user sees all teams and answers on org page' do
+      click_link organization.name
+
+      expect(page).to have_content(team.name)
+      expect(page).to have_content(new_team.name)
+    end
+
+end
