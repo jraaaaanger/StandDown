eff767b5bfae8ef32383a961fd06008d067404dc
Jimmy Ranger <jranger@me.com>
2016-08-27 15:51:49 -0400

    these are all going to be deleted

--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import orgFeed from './components/orgFeed';
 
 $(function() {
   ReactDOM.render(
-    <h1>Test</h1>,
+    <orgFeed />,
     document.getElementById('app')
   );
 });
